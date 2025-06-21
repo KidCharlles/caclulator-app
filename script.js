e22240586bb3253d4968c50f9cb2434428671d68
KidCharles <sircharlyrivera@gmail.com>
2025-06-21 10:50:12 -0400

    adding arithmetic operations functionality

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+// helpter function to get and convert current input values as numbers
+function getInputs() {
+    const input1= Number(document.getElementById("number-1").value);
+    const input2 = Number(document.getElementById("number-2").value);
+    return { input1, input2};
+}
+
+// Addition
 document.getElementById("add").addEventListener("click", function() {
-    let input1= document.getElementById("number-1").value;
-    console.log(input1)
+    const { input1, input2 } = getInputs(); // object destructuring:
+    console.log(input1 + input2); 
+})
+
+// Subtraction
+document.getElementById("subtract").addEventListener("click", function() {
+    const {input1, input2} = getInputs();
+    console.log(input1 - input2);
+})
+
+// Division
+document.getElementById("divide").addEventListener("click", function() {
+    const {input1, input2} = getInputs();
+    console.log(input1 / input2);
+})
+
+// Multiplication
+document.getElementById("multiply").addEventListener("click", function() {
+    const {input1, input2} = getInputs();
+    console.log(input1 * input2);
 })
